df674962becaec893f1c26d64a7f7f1904031bfc
Maryx23 <maryxxyron.corpuz@lorma.edu>
2026-02-13 15:29:13 +0800

    activity1

---
+++ Activity1/Week2/Book.java
@@ -0,0 +1,14 @@
+package Activity.Week2;
+
+public class Book  {
+    String title;
+    String author;
+    String isbn; 
+    String publicationdate;
+   
+    public void printBookInfo() {
+        System.out.println("\n" + title  + "\n" + author + "\n" + isbn + "\n" + publicationdate + "\n");
+    
+    
+    }
+}
